17271d7d43827e92620fac46d4c707f279c8dad4
Ben Wright <bmw066000@gmail.com>
2012-12-14 08:02:33 -0600

    Episode 13
    Loading Levels

--- src/com/bwright/game/Game.java
+++ src/com/bwright/game/Game.java
@@ -53,6 +53,7 @@ public class Game extends Canvas implements Runnable {
 		frame.add(this, BorderLayout.CENTER);
 		frame.pack();
 
+		requestFocus();
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
@@ -74,7 +75,7 @@ public class Game extends Canvas implements Runnable {
 
 		screen = new Screen(WIDTH, HEIGHT, new SpriteSheet("/sprite_sheet.png"));
 		input = new InputHandler(this);
-		level = new Level(64, 64);
+		level = new Level("/levels/small_test_level.png");
 		player = new Player(level, 0, 0, input);
 		level.addEntity(player);
 	}
--- src/com/bwright/game/level/tiles/BasicSolidTile.java
+++ src/com/bwright/game/level/tiles/BasicSolidTile.java
@@ -2,8 +2,8 @@ package com.bwright.game.level.tiles;
 
 public class BasicSolidTile extends BasicTile {
 
-	public BasicSolidTile(int id, int x, int y, int tileColor) {
-		super(id, x, y, tileColor);
+	public BasicSolidTile(int id, int x, int y, int tileColor, int levelColor) {
+		super(id, x, y, tileColor, levelColor);
 		this.solid = true;
 	}
 
